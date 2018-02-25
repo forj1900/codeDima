253d0ba26d63149c4ce49a2e663bde1c6c1c8f6b
dimazh2002 <dimaz2002@icloud.com>
2018-02-25 13:39:40 +0300

    initial commint

---
+++ code.py
@@ -0,0 +1,13 @@
+alphabet1 = "a", "b", "c"
+alphabet2 = 1, 2, 3
+print (alphabet1[0])
+print (alphabet2[0])
+
+
+sentenceSource = "abc"
+sentenceCode = "   "
+
+print(sentenceSource)
+print (sentenceCode)
+
+letter = 
